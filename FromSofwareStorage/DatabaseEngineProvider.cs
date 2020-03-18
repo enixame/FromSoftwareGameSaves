ddf3058828952727bd28314020a693a5f27f116e
Maxime Leonard <maximeleonard@hotmail.fr>
2020-03-18 19:28:06 +0100

    Fix cjproject naming + Add RsaCryptoService

---
+++ FromSofwareStorage/DatabaseEngineProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlServerCe;
+using System.IO;
+
+namespace FromSoftwareStorage
+{
+    public sealed class DatabaseEngineProvider
+    {
+        public static readonly string AppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FromSoftwareGameSaves", "Storage");
+        private const string DatabaseName = "Data.sdf";
+        private const string RsaPrivateKeyFile = "DataPrivateKey.xml";
+
+        public DatabaseEngineProvider()
+        {
+            DatabaseFileName = Path.Combine(AppDataPath, DatabaseName);
+            RsaPrivateKeyFileName = Path.Combine(AppDataPath, RsaPrivateKeyFile);
+        }
+
+        public string DatabaseFileName { get; }
+        public string RsaPrivateKeyFileName { get; }
+
+        public void CreateDatabase(string password)
+        {
+            string connectionString = $"Data Source='{DatabaseFileName}'; LCID=1033; Password={password}; Case Sensitive = TRUE;";
+            DoCreation(connectionString);
+
+            EncryptPassword(password);
+        }
+
+        private void EncryptPassword(string password)
+        {
+            RsaCryptoService.GeneratePrivateKey(RsaPrivateKeyFileName);
+
+
+        }
+
+        public void CreateDatabase()
+        {
+            string connectionString = $"Data Source='{DatabaseFileName}'; LCID=1033; Case Sensitive = TRUE;";
+            DoCreation(connectionString);
+        }
+
+        private static void DoCreation(string connectionString)
+        {
+            SqlCeEngine engine = new SqlCeEngine(connectionString);
+            engine.CreateDatabase();
+        }
+    }
+}
---
+++ FromSofwareStorage/RsaCryptoService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FromSoftwareStorage
+{
+    internal sealed class RsaCryptoService : IDisposable
+    {
+        private const int DwKeySize = 2048;
+
+        private readonly RSACryptoServiceProvider _rsaCryptoServiceProvider;
+        private readonly UnicodeEncoding _byteConverter = new UnicodeEncoding();
+
+        internal static void GeneratePrivateKey(string path)
+        {
+            using (RSACryptoServiceProvider rsaCryptoServiceProvider = new RSACryptoServiceProvider(DwKeySize))
+                System.IO.File.WriteAllText(path, rsaCryptoServiceProvider.ToXmlString(true));
+        }
+
+        public RsaCryptoService()
+        {
+            _rsaCryptoServiceProvider = new RSACryptoServiceProvider(DwKeySize);
+        }
+
+        public string EncryptData(string path, string data)
+        {
+            string base64String = null;
+            try
+            {
+                byte[] dataToEncrypt = _byteConverter.GetBytes(data);
+
+                _rsaCryptoServiceProvider.FromXmlString(System.IO.File.ReadAllText(path));
+                byte[] encryptedData = _rsaCryptoServiceProvider.Encrypt(dataToEncrypt, true);
+                base64String = Convert.ToBase64String(encryptedData);
+            }
+            finally
+            {
+                _rsaCryptoServiceProvider.PersistKeyInCsp = false;
+            }
+
+            return base64String;
+        }
+
+        public string DecryptData(string path, string data)
+        {
+            string decryptedData = null;
+            try
+            {
+                _rsaCryptoServiceProvider.FromXmlString(System.IO.File.ReadAllText(path));
+                byte[] base64String = Convert.FromBase64String(data);
+                byte[] bytes = _rsaCryptoServiceProvider.Decrypt(base64String, true);
+
+                decryptedData = _byteConverter.GetString(bytes);
+            }
+            finally
+            {
+                _rsaCryptoServiceProvider.PersistKeyInCsp = false;
+            }
+
+            return decryptedData;
+        }
+
+        public void Dispose()
+        {
+            _rsaCryptoServiceProvider?.Dispose();
+        }
+    }
+}
